e73fbe624b2ef121d1ebe73ef46a221f9351fb6d
Cid <abhishekmondol378@gmail.com>
2025-12-24 20:10:34 +0600

    Create responses.py

---
+++ chatbot/responses.py
@@ -0,0 +1,14 @@
+responses = {
+    "hello": ["Hi there!", "Hello!", "Hey! How can I help you?"],
+    "hi": ["Hi!", "Hello!", "Hey there!"],
+    "bye": ["Goodbye!", "See you later!"],
+    "thanks": ["You're welcome!", "No problem!"],
+    "name": ["I am a simple rule-based chatbot."],
+    "age": ["I don't have an age. I'm a program."],
+}
+
+default_responses = [
+    "I'm not sure how to respond to that.",
+    "Can you rephrase?",
+    "I don't understand.",
+]
